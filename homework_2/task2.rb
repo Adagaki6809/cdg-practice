90536e5dc4f2393024bd1af16ea4e89720a74090
Pavel Berchikov <include.ofstream.h@gmail.com>
2022-04-08 16:33:05 +0300

    Rewrite method and tests

--- homework_2/task2.rb
+++ homework_2/task2.rb
@@ -1,27 +1,17 @@
 # frozen_string_literal: true
 
-def get_pokemons(number, names, colors)
+def get_pokemons(number)
   pokemons = []
-  number.times do |i|
-    pokemons << { name: names[i], color: colors[i] }
+  number.times do
+    print 'Введите имя: '
+    name = gets.chomp
+    print 'Введите цвет: '
+    color = gets.chomp
+    pokemons << { name:, color: }
   end
   pokemons
 end
 
-def get_names_and_colors(number)
-  names = []
-  colors = []
-  number.times do
-    print 'Введите имя: '
-    names << gets.chomp
-    print 'Введите цвет: '
-    colors << gets.chomp
-  end
-  [names, colors]
-end
-
 print 'Введите кол-во покемонов: '
 number = gets.to_i
-names, colors = get_names_and_colors(number)
-result = get_pokemons(number, names, colors)
-print result
+print get_pokemons(number)
